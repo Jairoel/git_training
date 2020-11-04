2b717326ec1b34ae3c46d3fe604f157ecada3eca
sjo <jairo.egea.7e4@itb.cat>
2020-11-04 10:39:14 +0100

    Practica

---
+++ training/if.java
@@ -0,0 +1,12 @@
+public static class iftraining
+	Public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+		int n = scanner.nextInt;
+
+		if(n >= 5)
+			System.out.println("Aprobat");
+		else
+			System.out.println("Suspés");
+	}
+}
